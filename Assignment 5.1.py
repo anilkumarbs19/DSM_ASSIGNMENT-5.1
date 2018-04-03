a4b0d85860c8b4a9096ff848174d9aa72c7bfd0f
anilkumarbs19 <anilkumarbs19@gmail.com>
2018-04-03 20:36:11 +0530

    Minor changes

--- Assignment 5.1.py
+++ Assignment 5.1.py
@@ -4,15 +4,16 @@
 # In[28]:
 
 
-print("\n Assignment complete\n")
+
 def div():
     return 5/0
-    try:
-        div()
-    except ZeroDivisionError:
-        print ("division by zero!")
-    except Exceptionerror:
-        print('there is an exception')
-    finally:
-        print('execution done')
+    
+try:
+	div()
+except ZeroDivisionError:
+	print ("division by zero!")
+except Exceptionerror:
+	print('there is an exception')
+finally:
+	print('execution done')
 
